f8dcf470fa9f344abfca358744849c91586ac08e
Kiryl Stetsenka <kiryl.stetsenka@innowise-group.com>
2022-10-07 14:18:39 +0000

    Add new file

---
+++ dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#sudo docker-compose up -d --build
+echo "Write a hash, which helps connect slave to master as dev"
+read hash
+
+sudo docker run -itd --name dev --network $(hostname)_jenkins 123
+sudo docker exec -it dev curl -sO http://172.19.0.2:8080/jnlpJars/agent.jar
+sudo docker exec -it dev nano /usr/share/maven/conf/settings.xml
+sudo docker exec -i dev  java -jar agent.jar -jnlpUrl http://172.19.0.2:8080/computer/dev/jenkins-agent.jnlp -secret $hash &
