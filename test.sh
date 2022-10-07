eaac19874452e63ab5066728ebd0a376e047c1fe
Kiryl Stetsenka <kiryl.stetsenka@innowise-group.com>
2022-10-07 14:19:02 +0000

    Add new file

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Write a hash, which helps connect slave to master as dev"
+read hashik
+
+sudo docker run -itd --name test --network $(hostname)_jenkins 123
+sudo docker exec -it test curl -sO http://172.19.0.2:8080/jnlpJars/agent.jar
+sudo docker exec -it test nano /usr/share/maven/conf/settings.xml
+sudo docker exec -i test java -jar agent.jar -jnlpUrl http://172.19.0.2:8080/computer/test/jenkins-agent.jnlp -secret $hashik &
