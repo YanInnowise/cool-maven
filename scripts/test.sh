d9202eb2715aea8fd1c7eef29cd701124587dbc5
hencezze <104189173+hencezze@users.noreply.github.com>
2022-10-08 15:40:28 +0300

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,3 +5,6 @@ read hashik
 sudo docker run -itd --name test --network $(hostname)_jenkins slave
 sudo docker exec -it test curl -sO http://172.19.0.2:8080/jnlpJars/agent.jar
 sudo docker exec -i test java -jar agent.jar -jnlpUrl http://172.19.0.2:8080/computer/test/jenkins-agent.jnlp -secret $hashik &
+
+echo "Password for get first access into Nexus"
+sudo docker exec -it nexus cat /nexus-data/admin.password
